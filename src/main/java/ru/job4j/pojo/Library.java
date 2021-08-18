c4861e8391cb9cd861360339293800953b734db2
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-08-18 16:00:14 +0300

    3. Массивы и модели. [#174892 #191073] fix

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -18,7 +18,7 @@ public class Library {
         }
         System.out.println();
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println("Name: " + book.getName() + ", pages: " + book.getPages());
             }
         }
