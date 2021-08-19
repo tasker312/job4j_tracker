3cabb016b572d57da177a466d01d68135645c775
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-08-19 09:37:58 +0300

    3. Интерфейс Input [#181092 #191432] fix

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Автобус едет по дороге");
     }
 
     @Override
     public void passengers(int amount) {
-
+        System.out.println("В автобусе " + amount + " пассажиров");
     }
 
     @Override
     public double fill(double amount) {
-        return 0;
+        return amount * 38.95;
     }
 }
