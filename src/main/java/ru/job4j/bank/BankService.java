1f7b0d7f891072f7f6af4d0c5bab68a8293a5cb2
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-08-27 18:29:02 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу банковской системы
+ * @author Tasker
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Хранение списка пользователей осуществляется в HashMap<User, ArrayList<Account>>
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод позволяет добавить нового пользователя банковскую систему
+     * @param user пользователь
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод позволяет добавить новый счет пользователю банковской системы,
+     * если пользователь существует, а его счета с такими реквизитами не существует
+     * @param passport паспорт пользователя
+     * @param account счет
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user == null) {
@@ -23,6 +41,12 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод позволяет найти пользователя в банковской системе по его паспорту
+     * @param passport паспорт пользователя
+     * @return возвращает пользователя, если пользователь с таким паспортом
+     * существует в системе, иначе null
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
@@ -32,6 +56,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяет найти счет по его паспорту и реквизитам счета
+     * @param passport паспорт пользователя
+     * @param requisite реквизиты счета
+     * @return счет, если пользователь с таким счетом существует, иначе null
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user == null) {
@@ -45,6 +75,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяет выполнить операцию перевода с одного счета на другой
+     * @param srcPassport паспорт пользователя (источник)
+     * @param srcRequisite реквизиты счета пользователя (источник)
+     * @param destPassport паспорт пользователя (назначение)
+     * @param destRequisite реквизиты счета пользователя (назначение)
+     * @param amount сумма для перевода
+     * @return true, если счета существуют и на счете источнике находится достаточная
+     * сумма для перевода; false в других случаях
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,69 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает пользователя
+ * @author Tasker
+ * @version 1.0
+ */
 public class User {
+    /**
+     * Паспорт пользователя
+     */
     private String passport;
+
+    /**
+     * Имя пользователя
+     */
     private String username;
 
+    /**
+     * Метод создает нового пользователя, принимает на вход его паспорт и имя
+     * @param passport паспорт пользователя
+     * @param username имя пользователя
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод позволяет получить паспорт пользователя
+     * @return возвращает паспорт пользователя
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод позволяет изменить паспорт пользователя, принимает на вход новый паспорт
+     * @param passport паспорт пользователя
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод позволяет получить имя пользователя
+     * @return возвращает имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод позволяет изменить имя пользователя, принимает на вход новое имя
+     * @param username имя пользователя
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Метод сравнивает пользователей по их паспортам
+     * @param o пользователь для сравнения
+     * @return true, если паспорта совпадают; false в других случаях
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +77,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Метод позволяет получить хэш код объекта
+     * @return хэш код объекта
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
